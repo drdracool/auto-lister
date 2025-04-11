b762ada20ffb5eed3f524da2d29f5694748c7e0e
drdracool <jadexing@foxmail.com>
2025-04-11 19:26:43 +0900

    Create table, product model and insert function

--- repository/product_repo.py
+++ repository/product_repo.py
@@ -1,16 +1,74 @@
 import sqlite3
+from model.product import Product
+import datetime
 
 
 class Product_Repo:
-    def __init__(self, path):
-        self.initialize(path)
+    def __init__(self, path: str):
+        self.DB_PATH: str = path
+        self.initialize()
 
-    def initialize(self, path):
+    def initialize(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS product (
+          id INTEGER PRIMARY KEY,
+          name TEXT NOT NULL,
+          url TEXT NOT NULL,
+          regular_price INTEGER NOT NULL,
+          selling_price INTEGER NOT NULL,
+          discount_amount INTEGER NOT NULL,
+          discount_percentage REAL NOT NULL,
+          image_url TEXT NOT NULL,
+          description TEXT NOT NULL,
+          stock INTEGER NOT NULL,
+          additional_image_urls TEXT,
+          publish_status INTEGER NOT NULL,
+          short_name TEXT NOT NULL,
+          short_description TEXT NOT NULL,
+          modified_date TIMESTAMP
+        )
+      """
+
         try:
-            with sqlite3.connect(path) as conn:
+            with sqlite3.connect(self.DB_PATH) as conn:
                 print(
                     f"Opened SQLite database with version {sqlite3.sqlite_version} successfully."
                 )
+                cursor = conn.cursor()
+                cursor.execute(query)
+                conn.commit()
+                print("Table created successfully or table already existed.")
 
         except sqlite3.OperationalError as e:
             print("Failed to open database:", e)
+
+    def insert_product(
+        self,
+        product: Product,
+    ):
+        with sqlite3.connect(self.DB_PATH) as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                """
+            INSERT INTO products (name, url, regular_price, selling_price, discount_amount, 
+                                  discount_percentage, image_url, description, stock, additional_image_urls, publish_status, short_name, short_description, modified_date)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+                (
+                    product["name"],
+                    product["url"],
+                    product["regular_price"],
+                    product["selling_price"],
+                    product["discount_amount"],
+                    product["discount_percentage"],
+                    product["image_url"],
+                    product["description"],
+                    product["stock"],
+                    product["additional_image_urls"],
+                    product["publish_status"],
+                    product["short_name"],
+                    product["short_description"],
+                    datetime.datetime.now(),
+                ),
+            )
+            print(f"Product {product.short_name} has been successfully inserted!")
